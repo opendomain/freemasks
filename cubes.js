3c71d1b55f7d0bf87849734359f85e7d8b81e411
Ric Johnson <ric@nosql.com>
2021-01-17 17:12:18 -0500

    Create cubes.js

---
+++ cubes.js
@@ -0,0 +1,436 @@
+var _baseset = {
+  "cubes": [
+    {
+      "visible": true,
+      "objRef": null,
+      "lineRef": null,
+      "order": 0,
+      "position": {
+        "x": 2,
+        "y": 1,
+        "z": 1
+      },
+      "rotation": {
+        "x": 0,
+        "y": 0,
+        "z": 0
+      },
+      "faces": [
+        {
+          "id": 0,
+          "image": "half",
+          "imageOrientation": 270,
+          "color": "#ff0000"
+        },
+        {
+          "id": 1,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#00ff00"
+        },
+        {
+          "id": 2,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#ffffff"
+        },
+        {
+          "id": 3,
+          "image": "half",
+          "imageOrientation": 180,
+          "color": "#f0db4f"
+        },
+        {
+          "id": 4,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#ffffff"
+        },
+        {
+          "id": 5,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "visible": true,
+      "objRef": null,
+      "lineRef": null,
+      "order": 1,
+      "position": {
+        "x": 2,
+        "y": 1,
+        "z": 2
+      },
+      "rotation": {
+        "x": 0,
+        "y": 0,
+        "z": 0
+      },
+      "faces": [
+        {
+          "id": 0,
+          "image": "block",
+          "imageOrientation": 0,
+          "color": "#ff0000"
+        },
+        {
+          "id": 1,
+          "image": "triangle",
+          "imageOrientation": 180,
+          "color": "#00ff00"
+        },
+        {
+          "id": 2,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#ffffff"
+        },
+        {
+          "id": 3,
+          "image": "half",
+          "imageOrientation": 90,
+          "color": "#f0db4f"
+        },
+        {
+          "id": 4,
+          "image": "block",
+          "imageOrientation": 0,
+          "color": "#0000ff"
+        },
+        {
+          "id": 5,
+          "image": "triangle",
+          "imageOrientation": 270,
+          "color": "#ff00ff"
+        }
+      ]
+    },
+    {
+      "visible": true,
+      "objRef": null,
+      "lineRef": null,
+      "order": 2,
+      "position": {
+        "x": 2,
+        "y": 0,
+        "z": 2
+      },
+      "rotation": {
+        "x": 0,
+        "y": 0,
+        "z": 0
+      },
+      "faces": [
+        {
+          "id": 0,
+          "image": "half",
+          "imageOrientation": 0,
+          "color": "#ff0000"
+        },
+        {
+          "id": 1,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#00ff00"
+        },
+        {
+          "id": 2,
+          "image": "half",
+          "imageOrientation": 90,
+          "color": "#ffffff"
+        },
+        {
+          "id": 3,
+          "image": "half",
+          "imageOrientation": 180,
+          "color": "#f0db4f"
+        },
+        {
+          "id": 4,
+          "image": "block",
+          "imageOrientation": 0,
+          "color": "#0000ff"
+        },
+        {
+          "id": 5,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#ff00ff"
+        }
+      ]
+    },
+    {
+      "visible": true,
+      "objRef": null,
+      "lineRef": null,
+      "order": 3,
+      "position": {
+        "x": 2,
+        "y": 0,
+        "z": 1
+      },
+      "rotation": {
+        "x": 0,
+        "y": 0,
+        "z": 0
+      },
+      "faces": [
+        {
+          "id": 0,
+          "image": "block",
+          "imageOrientation": 0,
+          "color": "#ff0000"
+        },
+        {
+          "id": 1,
+          "image": "triangle",
+          "imageOrientation": 0,
+          "color": "#00ff00"
+        },
+        {
+          "id": 2,
+          "image": "half",
+          "imageOrientation": 0,
+          "color": "#ffffff"
+        },
+        {
+          "id": 3,
+          "image": "half",
+          "imageOrientation": 270,
+          "color": "#f0db4f"
+        },
+        {
+          "id": 4,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#0000ff"
+        },
+        {
+          "id": 5,
+          "image": "triangle",
+          "imageOrientation": 180,
+          "color": "#ff00ff"
+        }
+      ]
+    },
+    {
+      "visible": true,
+      "objRef": null,
+      "lineRef": null,
+      "order": 4,
+      "position": {
+        "x": 1,
+        "y": 1,
+        "z": 1
+      },
+      "rotation": {
+        "x": 0,
+        "y": 0,
+        "z": 0
+      },
+      "faces": [
+        {
+          "id": 0,
+          "image": "half",
+          "imageOrientation": 90,
+          "color": "#ff0000"
+        },
+        {
+          "id": 1,
+          "image": "triangle",
+          "imageOrientation": 90,
+          "color": "#00ff00"
+        },
+        {
+          "id": 2,
+          "image": "half",
+          "imageOrientation": 270,
+          "color": "#ffffff"
+        },
+        {
+          "id": 3,
+          "image": "half",
+          "imageOrientation": 270,
+          "color": "#f0db4f"
+        },
+        {
+          "id": 4,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#ffffff"
+        },
+        {
+          "id": 5,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "visible": true,
+      "objRef": null,
+      "lineRef": null,
+      "order": 5,
+      "position": {
+        "x": 1,
+        "y": 1,
+        "z": 2
+      },
+      "rotation": {
+        "x": 0,
+        "y": 0,
+        "z": 0
+      },
+      "faces": [
+        {
+          "id": 0,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#ff0000"
+        },
+        {
+          "id": 1,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#00ff00"
+        },
+        {
+          "id": 2,
+          "image": "half",
+          "imageOrientation": 180,
+          "color": "#ffffff"
+        },
+        {
+          "id": 3,
+          "image": "half",
+          "imageOrientation": 0,
+          "color": "#f0db4f"
+        },
+        {
+          "id": 4,
+          "image": "block",
+          "imageOrientation": 0,
+          "color": "#0000ff"
+        },
+        {
+          "id": 5,
+          "image": "triangle",
+          "imageOrientation": 0,
+          "color": "#ff00ff"
+        }
+      ]
+    },
+    {
+      "visible": true,
+      "objRef": null,
+      "lineRef": null,
+      "order": 6,
+      "position": {
+        "x": 1,
+        "y": 0,
+        "z": 2
+      },
+      "rotation": {
+        "x": 0,
+        "y": 0,
+        "z": 0
+      },
+      "faces": [
+        {
+          "id": 0,
+          "image": "half",
+          "imageOrientation": 180,
+          "color": "#ff0000"
+        },
+        {
+          "id": 1,
+          "image": "triangle",
+          "imageOrientation": 270,
+          "color": "#00ff00"
+        },
+        {
+          "id": 2,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#ffffff"
+        },
+        {
+          "id": 3,
+          "image": "half",
+          "imageOrientation": 90,
+          "color": "#f0db4f"
+        },
+        {
+          "id": 4,
+          "image": "block",
+          "imageOrientation": 0,
+          "color": "#0000ff"
+        },
+        {
+          "id": 5,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#ff00ff"
+        }
+      ]
+    },
+    {
+      "visible": true,
+      "objRef": null,
+      "lineRef": null,
+      "order": 7,
+      "position": {
+        "x": 1,
+        "y": 0,
+        "z": 1
+      },
+      "rotation": {
+        "x": 0,
+        "y": 0,
+        "z": 0
+      },
+      "faces": [
+        {
+          "id": 0,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#ff0000"
+        },
+        {
+          "id": 1,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#00ff00"
+        },
+        {
+          "id": 2,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#ffffff"
+        },
+        {
+          "id": 3,
+          "image": "half",
+          "imageOrientation": 0,
+          "color": "#f0db4f"
+        },
+        {
+          "id": 4,
+          "image": "blank",
+          "imageOrientation": 0,
+          "color": "#0000ff"
+        },
+        {
+          "id": 5,
+          "image": "triangle",
+          "imageOrientation": 90,
+          "color": "#ff00ff"
+        }
+      ]
+    }
+  ]
+};

83dd209dea44cac59e42a58800a84036b460160a
Ric Johnson <ric@nosql.com>
2021-01-28 23:30:02 -0500

    Update canvas.js

--- script/canvas.js
+++ script/canvas.js
@@ -1,3 +1,4 @@
+
 var _canvasColor = "aqua";
 
 var _elemCanvas;
@@ -5,9 +6,15 @@ var _canvasWidth;
 var _canvasHeight;
 var _ctx;
 
+var _canvasColor2 = "teal";
+
+var _elemCanvas2;
+var _canvasWidth2;
+var _canvasHeight2;
+var _ctx2;
+
 function getCanvas() {
-    var canvases = document.getElementsByTagName("canvas");
-    _elemCanvas = canvases[0];
+    var _elemCanvas = document.getElementById("canvasElem");
 
     if (_elemCanvas) {
         _canvasWidth = _elemCanvas.width;
@@ -20,4 +27,19 @@ function getCanvas() {
         _ctx.rect(0, 0, _canvasWidth, _canvasHeight);
         _ctx.fill();
     }
+
+    var _elemCanvas2 = document.getElementById("canvasElem2");
+
+    if (_elemCanvas2) {
+        _canvasWidth2 = _elemCanvas2.width;
+        _canvasHeight2 = _elemCanvas2.height;
+
+        _ctx2 = _elemCanvas2.getContext('2d');
+        _ctx2.clearRect(0, 0, _canvasWidth2, _canvasHeight2);
+
+        _ctx2.fillStyle = _canvasColor2;
+        _ctx2.rect(0, 0, _canvasWidth2, _canvasHeight2);
+        _ctx2.fill();
+    }
+
 }
